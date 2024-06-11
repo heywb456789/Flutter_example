a146f8ca071397afdaeb033a9300e49de7825dd3
heywb456789 <heywb457@naver.com>
2024-06-11 10:20:52 +0900

    done

--- toonflix/README.md
+++ toonflix/README.md
@@ -17,3 +17,12 @@ https://chocolatey.org/install
 2. simulator
 
 android studio 세팅.
+
+# vscode dart DX 세팅
+
+1. 자동 Const 변환  
+   "editor.codeActionsOnSave": {
+   "source.fixAll": "explicit"
+   },
+2. 계층 구조에 대한 시각화
+   "dart.previewFlutterUiGuides": true
--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -6,23 +6,26 @@ void main() {
   // runApp 은 Widget을 사용하여 앱에 주입 가능
   // Widget 은 UI 기능을 담은 레고블록 같은 것
   // widget 은 programming 적으론 class
-  runApp(App());
+  runApp(const App());
 }
 
 // 이 위젯이 루트가 된다.
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       //앱의 스타일 MaterialApp : Android // CupertinoApp: IOS
       //scaffold :
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 80,
               ),
               Row(
@@ -31,7 +34,7 @@ class App extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text(
+                      const Text(
                         'Hey Selina',
                         style: TextStyle(
                           color: Colors.white,
@@ -47,9 +50,48 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
-                  )
+                  ),
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text('Total Balance',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                  )),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text('\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 44,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  )),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
